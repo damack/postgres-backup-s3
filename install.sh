510bba032aa2e1ed065bc67b2baa13a4edcc4da8
damack <damack@users.noreply.github.com>
2021-04-19 19:23:22 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# exit if a command fails
+set -e
+
+apk update
+
+# install pg_dump
+apk add postgresql
+
+# install s3 tools
+apk add python3 py3-pip
+pip3 install awscli
+apk del py3-pip
+
+# cleanup
+rm -rf /var/cache/apk/*
